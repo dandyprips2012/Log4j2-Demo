4244864f184af51f136b913d67b3f6d37a0e2244
Priyanshu Borthakur <priyanshu.borthakur@zensar.com>
2018-02-15 04:06:14 -0800

    Changed log4j2 dependency. multi converter key and code modified.

--- Sample-Log4j2/src/main/java/org/practice/log4j2/cpl/CustomPatternConverter.java
+++ Sample-Log4j2/src/main/java/org/practice/log4j2/cpl/CustomPatternConverter.java
@@ -11,33 +11,42 @@ import org.apache.logging.log4j.core.pattern.ConverterKeys;
 import org.apache.logging.log4j.core.pattern.LogEventPatternConverter;
 
 @Plugin(category = "Converter", name = "TestPatternConverter")
-@ConverterKeys(value = { "ct" })
+@ConverterKeys(value = { "ct" , "ua"})
 public class CustomPatternConverter extends LogEventPatternConverter{
 
-	protected CustomPatternConverter(String name, String style) {
-		super(name, style);
+	private static Map<String, String> patternMap = new HashMap<String, String>(10);
+	private static int beginIndex = 0;
+
+	protected CustomPatternConverter(String [] options) {
+		super("name", "style");
 	}
 
 	public static CustomPatternConverter newInstance(String [] options) {
-		return new CustomPatternConverter("ct","ct");
+		return new CustomPatternConverter(options);
 
 	}
-	
+
 	@Override
 	public void format(LogEvent le, StringBuilder sb) {
-		sb.append(parseMessage(le.getMessage().toString().subSequence(22, le.getMessage().toString().length()-1).toString()));
+		if(patternMap.isEmpty())
+			this.createMap(le.getMessage().getFormattedMessage());
+		if(Pattern.compile("content-type").matcher(sb.substring(beginIndex)).find()) {
+			sb.append(patternMap.get("Content-Type"));
+			beginIndex = sb.indexOf(patternMap.get("Content-Type"));
+		} else if (Pattern.compile("User-Agent").matcher(sb).find()) {
+			sb.append(patternMap.get("User-Agent"));
+			beginIndex = sb.indexOf(patternMap.get("User-Agent"));
+		}
 	}
 
-	private String parseMessage(String s) {
-		Map<String, String> patternMap = new HashMap<String, String>(10);
+	private void createMap(String formatMessage) {
 		String pattern = "(\\S+):\\s(\\S*)(?:\\b(?!:)|$)";
 		Pattern p = Pattern.compile(pattern);
-		Matcher m = p.matcher(s);
-		
+		Matcher m = p.matcher(formatMessage);
+
 		while(m.find()) {
 			patternMap.put(m.group(1), m.group(2));
 		}
-		return patternMap.get("Content-Type");
 	}
 
 }
